dc6ccc89b3a4d6a9f20691125da365df123f487e
Mohamed_Abdelwahid <2000sara.mohamed@gmail.com>
2026-05-06 19:53:34 +0300

    create notes app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Notesapp());
+}
+// ignore: camel_case_types
+class Notesapp extends StatelessWidget {
+  const Notesapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
