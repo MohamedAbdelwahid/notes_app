ae10445628b9f3221cf1792460c5cd1fb2a33648
Mohamed_Abdelwahid <2000sara.mohamed@gmail.com>
2026-05-08 23:39:20 +0300

    add floatin action botton

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -3,14 +3,13 @@ import 'package:frist_project/views/widgets/custom_note_item.dart';
 
 class NotesListVeiw extends StatelessWidget {
   const NotesListVeiw({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return const Padding(
-          padding:  EdgeInsets.symmetric(vertical: 8),
-          child:  NoteItem(),
+          padding: EdgeInsets.symmetric(vertical: 4),
+          child: NoteItem(),
         );
       },
     );
