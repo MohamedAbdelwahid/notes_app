b372b8d7df38ee8ebb781dc2be01399ecf73537c
Mohamed_Abdelwahid <2000sara.mohamed@gmail.com>
2026-05-28 16:30:06 +0300

    init hive and open notes box

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
 const Color kprimaryColor = Color(0xff62FCD7);
+
+const knotesBox = 'notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:frist_project/constant.dart';
 import 'package:frist_project/views/notes_view.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(knotesBox);
   runApp(const Notesapp());
 }
+
 // ignore: camel_case_types
 class Notesapp extends StatelessWidget {
   const Notesapp({super.key});
@@ -12,10 +19,8 @@ class Notesapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,
-      fontFamily: 'Poppins'
-      ),
-      home:const NotesView(),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: const NotesView(),
     );
   }
 }
